f1ac7ff2de280090f125380062e8f0bb45831c48
Bence Mali <bencemali835@gmail.com>
2022-03-21 23:19:50 +0100

    small changes

--- buildrun.sh
+++ buildrun.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-clear && gcc -Wall -g -I ./ -c src/*.c && gcc -Wall -g -I ./ -o emulator *.o && rm *.o && ./emulator
+gcc -Wall -g -I./ -c src/*.c && gcc -Wall -g -I./ -o emulator *.o && rm *.o && ./emulator
